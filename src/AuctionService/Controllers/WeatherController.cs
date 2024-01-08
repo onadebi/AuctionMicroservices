ec81aeb73dbfe948023e4a5120fddd3490cc7f38
Onadebi <onadebi@onadebi.com>
2024-01-07 19:19:17 -0500

    Auction service implementation

---
+++ src/AuctionService/Controllers/WeatherController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+
+        [HttpGet("GetWeather")]
+        public IActionResult GetWeather()
+        {
+            WeatherForecast[] forecast = Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                Random.Shared.Next(20,50).ToString()
+            ))
+            .ToArray();
+            return Ok(forecast);
+        }
+    }
+}
